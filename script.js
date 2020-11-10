172276994b780a967f99d58fc2ec11714d6c4976
Cyril K. U <71171421+cyril1010@users.noreply.github.com>
2020-11-10 07:48:48 +0530

    Script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// THERE IS SOME ERROR WITH THIS CODE. I WILL DEFENITELY TRY TO CORRECT IT.
+
+// IF YOU CAN HELP ME WITH THIS. FEEL FREE TO CONNECT WITH ME ON INSTAGRAM @bboycyril_
+
 // Clear screen with C button.
 
 function clearScreen() {
